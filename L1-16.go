498de8ee9b03893e4b341d05165ea476a810ca5d
Ependi <khadzhiev909@gmail.com>
2026-02-26 03:08:30 +0300

    l1-16 done

--- L1-16.go
+++ L1-16.go
@@ -1,5 +1,27 @@
 package main
 
-func qsort(arr []int) {
+func qsort(arr []int) []int {
+	if len(arr) <= 1 {
+		return arr
+	}
 	pivot := arr[len(arr)/2]
+
+	left := []int{}
+	middle := []int{}
+	right := []int{}
+
+	for _, v := range arr {
+		switch {
+		case v < pivot:
+			left = append(left, v)
+		case v == pivot:
+			middle = append(middle, v)
+		default:
+			right = append(right, v)
+		}
+	}
+
+	result := append(qsort(left), middle...)
+	result = append(result, qsort(right)...)
+	return result
 }
