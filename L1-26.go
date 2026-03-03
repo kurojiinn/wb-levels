94007815c0b946495a4f391149a458cffd73128e
Ependi <khadzhiev909@gmail.com>
2026-03-03 18:52:51 +0300

    l1-26 done

--- L1-26.go
+++ L1-26.go
@@ -1,5 +1,7 @@
 package main
 
+import "unicode"
+
 //Разработать программу, которая проверяет, что все символы в строке встречаются один раз (т.е. строка состоит из уникальных символов).
 
 // Вывод: true, если все символы уникальны, false, если есть повторения. Проверка должна быть регистронезависимой, т.е. символы в разных регистрах считать одинаковыми.
@@ -7,7 +9,14 @@ package main
 // Например: "abcd" -> true, "abCdefAaf" -> false (повторяются a/A), "aabcd" -> false.
 
 // Подумайте, какой структурой данных удобно воспользоваться для проверки условия.
-// func hasDublicate(str string) {
-// 	unique := make(map[rune]struct{})
-
-// }
+func hasDuplicate(str string) bool {
+	unique := make(map[rune]struct{}, len(str))
+	for _, v := range str {
+		lowerV := unicode.ToLower(v)
+		if _, ok := unique[lowerV]; ok {
+			return false
+		}
+		unique[lowerV] = struct{}{}
+	}
+	return true
+}
